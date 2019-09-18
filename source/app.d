efe0c91abb751edd26b1decb09d11a1e3698eb21
Paul Backus <snarwin@gmail.com>
2019-09-18 15:18:47 -0400

    add generic adl wrapper

--- source/app.d
+++ source/app.d
@@ -1,10 +1,11 @@
 import traits;
 import tensor;
+import meta;
 
 extern (C) int main() {
   import std.typetuple : AliasSeq;
 
-  static foreach (m; AliasSeq!(traits, tensor))
+  static foreach (m; AliasSeq!(traits, tensor, meta))
     static foreach (u; __traits(getUnitTests, m))
       u();
 
--- source/meta.d
+++ source/meta.d
@@ -16,3 +16,31 @@ template adl(string fun, string moduleName = __MODULE__) {
     }
   }
 }
+
+struct AdlWrapper(T, string moduleName = __MODULE__) {
+  T obj;
+  alias obj this;
+
+  auto opDispatch(string method, Args...)(Args args) {
+    return obj.adl!(method, moduleName)(args);
+  }
+}
+
+AdlWrapper!(T, moduleName) adlWrap(T, string moduleName = __MODULE__)(T obj) {
+  return AdlWrapper!(T, moduleName)(obj);
+}
+
+version(unittest) {
+  struct S {}
+  bool empty(S s) { return false; }
+  int front(S s) { return 42; }
+  void popFront(S s) {}
+}
+
+unittest {
+  import std.range: take, only;
+  import std.algorithm: equal;
+
+  S s;
+  assert(s.adlWrap.take(5).equal(only(42, 42, 42, 42, 42)));
+}
